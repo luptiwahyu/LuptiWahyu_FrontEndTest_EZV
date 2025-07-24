139f23ed5aed8cd6bc6d31d9bb2d017e0b038ea6
Lupti Wahyu <lupti.utama@advotics.com>
2025-07-24 20:38:02 +0700

    feat: task list create update and delete

--- src/app/features/todos/pages/Main.tsx
+++ src/app/features/todos/pages/Main.tsx
@@ -6,8 +6,10 @@ import type { Task } from '../models/Task'
 const Main: FC = () => {
   const [newTask, setNewTask] = useState<string>('')
   const [taskList, setTaskList] = useState<Task[]>([])
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+  const [selectedTask, setSelectedTask] = useState<Task>({})
 
-  const addTask = (): void => {
+  const onAddTask = (): void => {
     if (newTask.trim() != '') {
       setTaskList([
         {
@@ -21,6 +23,27 @@ const Main: FC = () => {
     }
   }
 
+  const onEditTask = (task: Task): void => {
+    setIsEdit(true)
+    setSelectedTask(task)
+  }
+
+  const onSaveTask = (task: Task): void => {
+    setIsEdit(false)
+    setSelectedTask(task)
+  }
+
+  const onChangeTask = (value: string, id: number): void => {
+    const newTaskList = [...taskList]
+    const index = newTaskList.findIndex((task) => task.id === id)
+    newTaskList[index].name = value
+    setTaskList([...newTaskList])
+  }
+
+  const onRemoveTask = (id: number): void => {
+    setTaskList(taskList.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="p-4">
       <h1 className="text-2xl mb-4">To Do List</h1>
@@ -28,11 +51,12 @@ const Main: FC = () => {
       <div className="mb-4">
         <input
           type="text"
+          placeholder="Add new task"
           className="td-input mr-2"
           value={newTask}
           onInput={(e) => setNewTask(e.target.value)}
         />
-        <button className="td-button" disabled={!newTask} onClick={addTask}>
+        <button className="td-button" disabled={!newTask} onClick={onAddTask}>
           Add
         </button>
       </div>
@@ -42,8 +66,52 @@ const Main: FC = () => {
           {
             taskList.map((task) => (
               <li className="mb-4" key={task.id}>
-                <span className="mr-3">{task.name}</span>
-                <button className="td-button disabled">Remove</button>
+                <input
+                  id={`task-check-${task.id}`}
+                  type="checkbox"
+                  className="mr-3"
+                />
+
+                { isEdit ? (
+                  <input
+                    type="text"
+                    className="td-input mr-3"
+                    value={task.name}
+                    onChange={(e) => onChangeTask(e.target.value, task.id)}
+                  />
+                ) : (
+                  <label
+                    htmlFor={`task-check-${task.id}`}
+                    className="mr-3"
+                  >
+                    {task.name}
+                  </label>
+                )}
+
+
+                { isEdit ? (
+                  <button
+                    className="td-button mr-3"
+                    onClick={() => onSaveTask()}
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <button
+                    className="td-button mr-3"
+                    disabled={isEdit}
+                    onClick={() => onEditTask(task)}
+                  >
+                    Edit
+                  </button>
+                )}
+
+                <button
+                  className="td-button"
+                  onClick={() => onRemoveTask(task.id)}
+                >
+                  Remove
+                </button>
               </li>
             ))
           }
